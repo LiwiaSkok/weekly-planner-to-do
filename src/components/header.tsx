02d61baa82953ad74777a8d29a497cd1e0d9d945
LiwiaSkok <liwkaliwkas22@gmail.com>
2025-06-07 01:45:32 +0200

    Zmiana

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { Plus } from "lucide-react"; // <- import ikony
 export default function Header({ onAdd }: { onAdd: () => void }) {
   return (
     <header className="flex items-center justify-between p-4 border-b">
-      <h1 className="text-2xl font-bold text-gray-800">.Organizee</h1>
+      <h1 className="text-2xl font-bold text-gray-800">    .Organizee</h1>
       <button
         onClick={onAdd}
         className="w-12 h-12 rounded-full bg-pink-500 flex items-center justify-center shadow-md"
